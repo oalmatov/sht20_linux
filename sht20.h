4f93bece1b09330e7edab8bd3635e74110a23581
Omar Almatov <omar.almatov@gmail.com>
2019-06-30 16:32:08 +0600

    Initial commit

--- sht20.h
+++ sht20.h
@@ -1,7 +1,22 @@
-// sht20.h
+/* sht20.h
+****************************************************************************************************************
+****************************************************************************************************************
+    Copyright (C) 2019 Omar Almatov
+     
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+    Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+    
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+    License for more details.
+    
+    You should have received a copy of the GNU General Public License along with this program. If not, see
+    <http://www.gnu.org/licenses/>.
+*/
 
-#ifndef SHT20_H
-#define SHT20_H 1
+#ifndef SHT20_H_
+#define SHT20_H_ 1
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,4 +44,4 @@ protected:
     uint16_t    tempData_;
 };
 
-#endif // SHT20_H
+#endif // SHT20_H_
